33a9ff28db12edf4008299aacc9c8ae1e61afd10
EMO <jxzengyun@163.com>
2018-02-06 18:26:09 +0800

    add sqlite
    添加sqlite 方便后期数据管理

---
+++ sqlite-emo/sqlite-emo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+namespace sqlite_emo
+{
+    public partial class Form1 : Form
+    {
+        private SQLiteConnection m_dbConnection;
+        public Form1()
+        {
+            InitializeComponent();
+
+            createNewDatabase();
+            connectToDatabase();
+            createTable();
+            fillTable();
+            printHighscores();
+        }
+
+        //创建一个空的数据库
+        private void createNewDatabase()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.db");
+        }
+
+        //创建一个连接到指定数据库
+        private void connectToDatabase()
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.db;Version=3;");
+            m_dbConnection.Open();
+        }
+
+        //在指定数据库中创建一个table
+        private void createTable()
+        {
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //插入一些数据
+        private void fillTable()
+        {
+            string sql = "insert into highscores (name, score) values ('Me', 3000)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+        }
+
+        //使用sql查询语句，并显示结果
+        private void printHighscores()
+        {
+            string sql = "select * from highscores order by score desc";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sqlite-emo/sqlite-emo/common.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+namespace sqlite_emo
+{
+    class common
+    {
+        public static string Detect3264()
+        {
+            ConnectionOptions oConn = new ConnectionOptions();
+            System.Management.ManagementScope oMs = new System.Management.ManagementScope("\\\\localhost", oConn);
+            System.Management.ObjectQuery oQuery = new System.Management.ObjectQuery("select AddressWidth from Win32_Processor");
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher(oMs, oQuery);
+            ManagementObjectCollection oReturnCollection = oSearcher.Get();
+            string addressWidth = null;
+
+            foreach (ManagementObject oReturn in oReturnCollection)
+            {
+                addressWidth = oReturn["AddressWidth"].ToString();
+            }
+
+            return addressWidth;
+        }
+    }
+}
